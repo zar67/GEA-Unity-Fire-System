dceaf840217905502b99e2c770397362c1dc9c64
Zoe <zoe2.rowbotham@live.uwe.ac.uk>
2019-11-05 10:52:19 +0000

    Independent Fire Manager

--- GEA Fire System/Assets/FireAssets/FireRaycastProp.cs
+++ GEA Fire System/Assets/FireAssets/FireRaycastProp.cs
@@ -8,10 +8,8 @@ public class FireRaycastProp : MonoBehaviour
     public GameObject firePrefab;
     public GameObject flammableObjectsParent;
     public Transform fireParent;
-    public LayerMask fireLayerMask;
     public Material burntMaterial;
 
-    public int areaSize;
     public float spreadChance;
     public float burnTime;
 
@@ -106,48 +104,6 @@ public class FireRaycastProp : MonoBehaviour
                 }
             }
         }
-    }
-
-    /*void GenerateGrid()
-    {
-        for (int i = -areaSize / 2; i < areaSize / 2; i++)
-        {
-            for (int j = -areaSize / 2; j < areaSize / 2; j++)
-            {
-                GameObject temp_obj = GenerateParticle(i * particleDistance, transform.position.y, j * particleDistance);
-
-                if (i == 0 && j == 0)
-                {
-                    temp_obj.GetComponent<FireRaycastSpread>().Ignite(burnTime);
-                }
-
-                Collider[] colliders = Physics.OverlapSphere(temp_obj.transform.position, 1);
-                if (colliders.Length != 0)
-                {
-                    foreach (Collider collider in colliders)
-                    {
-                        if (!colliding_objects.Contains(collider.gameObject) && ((1 << collider.gameObject.layer) & fireLayerMask) != 0)
-                        {
-                            GenerateParticlesOnObject(collider);
-                        }
-                    }
-                }
-            }
-        }
-    }*/
-
-    void GenerateParticlesOnObject(GameObject obj)
-    {
-        Mesh mesh_filter = obj.GetComponent<MeshFilter>().mesh;
-        Vector3[] vertices = mesh_filter.vertices;
-
-        foreach (Vector3 vertex in vertices)
-        {
-            Vector3 world_pos = obj.transform.TransformPoint(vertex);
-            GenerateParticle(world_pos.x, world_pos.y, world_pos.z);
-        }
-
-        colliding_objects.Add(obj);
     }
 
     void GenerateParticlesOnObject(Collider obj)
